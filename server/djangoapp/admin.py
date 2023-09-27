bbace14da42e2907ef702e55a4f75bdc9e27f2d8
stierney0505 <stierney0505@gmail.com>
2023-09-27 01:41:46 -0400

    Nearly Completed Module 3 (this is a really vague commit message)

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,13 +1,14 @@
 from django.contrib import admin
 # from .models import related models
-
+from .models import *
 
 # Register your models here.
+class CarModelInline(admin.StackedInline):
+    model = CarModel
+    extra = 5
 
-# CarModelInline class
+class CarMakeAdmin(admin.ModelAdmin):
+    inlines = [CarModelInline]
 
-# CarModelAdmin class
-
-# CarMakeAdmin class with CarModelInline
-
-# Register models here
+admin.site.register(CarMake, CarMakeAdmin)
+admin.site.register(CarModel)
